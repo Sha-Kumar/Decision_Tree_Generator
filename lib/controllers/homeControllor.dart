d3564b04bf6041e39edd1b84dcdd1dceb3ea213f
rustiever <sharanzoff@gmail.com>
2020-11-01 21:25:02 +0530

    pagination is staterted in table view

--- lib/controllers/homeControllor.dart
+++ lib/controllers/homeControllor.dart
@@ -2,15 +2,37 @@ import 'dart:convert';
 
 import 'package:csv/csv.dart';
 import 'package:file_picker/file_picker.dart';
+
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   final RxInt indexValue = 0.obs;
+
+  RxInt start = 0.obs;
+  int limit = 200;
   int get index => indexValue.value;
   set index(int v) => indexValue.value = v;
+  ScrollController scrollController;
+  final RxBool isMoreAvailable = true.obs;
   RxList cols = <dynamic>[].obs;
   RxList<List<dynamic>> rows = <List<dynamic>>[].obs;
+  List<List<dynamic>> rowss = <List<dynamic>>[];
+
+  @override
+  void onInit() {
+    scrollController = ScrollController()
+      ..addListener(() {
+        if (scrollController.position.pixels >
+            // 90% scroll
+            0.6 * scrollController.position.maxScrollExtent) {
+          loadMore();
+          print('ini');
+        }
+      });
+    super.onInit();
+  }
 
   Future<void> loadFile(String mydata) async {
     // final mydata = await rootBundle.loadString("assets/baseball.csv");
@@ -19,8 +41,26 @@ class HomeController extends GetxController {
     ).convert(mydata);
     cols.value = csvTable.first;
     csvTable.removeAt(0);
-    rows.value = csvTable;
-    print(csvTable);
+    rowss = csvTable;
+    rows.addAll(
+      rowss.sublist(start.value, limit),
+    );
+    start.value += limit + 1;
+    // print(csvTable);
+  }
+
+  void loadMore() {
+    if (isMoreAvailable.value) {
+      int val = start.value + limit;
+      if (rowss.length <= val) {
+        val = rowss.length;
+        isMoreAvailable.value = false;
+      }
+      rows.addAll(
+        rowss.sublist(start.value, val),
+      );
+      start += limit + 1;
+    }
   }
 
   Future<void> openFileExplorer() async {
@@ -33,14 +73,7 @@ class HomeController extends GetxController {
           allowedExtensions: ['csv'],
           withData: true);
       final fg = res.files[0].bytes;
-      final asc = ascii.decode(fg);
-      if (asc.contains("\n")) {
-        print(asc.indexOf("\n"));
-      }
-      // print(String.fromCharCodes(fg));
       loadFile(ascii.decode(fg));
-
-      // print(utf8.decode(fg));
     } on PlatformException catch (e) {
       print("Unsupported operation$e");
     } catch (ex) {
@@ -48,3 +81,90 @@ class HomeController extends GetxController {
     }
   }
 }
+
+/*
+import 'dart:convert';
+
+import 'package:csv/csv.dart';
+import 'package:file_picker/file_picker.dart';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final RxInt indexValue = 0.obs;
+
+  RxInt start = 0.obs;
+  int limit = 200;
+  int get index => indexValue.value;
+  set index(int v) => indexValue.value = v;
+  ScrollController scrollController;
+  final RxBool isMoreAvailable = true.obs;
+  RxList cols = <dynamic>[].obs;
+  RxList<List<dynamic>> rows = <List<dynamic>>[].obs;
+  List<List<dynamic>> rowss = <List<dynamic>>[];
+
+  @override
+  void onInit() {
+    scrollController = ScrollController()
+      ..addListener(() {
+        if (scrollController.position.pixels >
+            // 90% scroll
+            0.6 * scrollController.position.maxScrollExtent) {
+          loadMore();
+          print('ini');
+        }
+      });
+    super.onInit();
+  }
+
+  Future<void> loadFile(String mydata) async {
+    // final mydata = await rootBundle.loadString("assets/baseball.csv");
+    final List<List<dynamic>> csvTable = const CsvToListConverter(
+      eol: "\n",
+    ).convert(mydata);
+    cols.value = csvTable.first;
+    csvTable.removeAt(0);
+    rowss = csvTable;
+    rows.addAll(
+      rowss.sublist(start.value, limit),
+    );
+    start.value += limit + 1;
+    // print(csvTable);
+  }
+
+  void loadMore() {
+    if (isMoreAvailable.value) {
+      int val = start.value + limit;
+      if (rowss.length <= val) {
+        val = rowss.length;
+        isMoreAvailable.value = false;
+      }
+      rows.addAll(
+        rowss.sublist(start.value, val),
+      );
+      start += limit + 1;
+    }
+  }
+
+  Future<void> openFileExplorer() async {
+    try {
+      final FilePickerResult res = await FilePicker.platform.pickFiles(
+          type: FileType.custom,
+          onFileLoading: (c) {
+            print(c.index);
+          },
+          allowedExtensions: ['csv'],
+          withData: true);
+      final fg = res.files[0].bytes;
+      loadFile(ascii.decode(fg));
+    } on PlatformException catch (e) {
+      print("Unsupported operation$e");
+    } catch (ex) {
+      print(ex);
+    }
+  }
+}
+
+   */
--- lib/views/homeview.dart
+++ lib/views/homeview.dart
@@ -9,6 +9,7 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
+          controller.start.value = 0;
           controller.openFileExplorer();
         },
         label: const Text('Upload'),
@@ -46,32 +47,37 @@ class HomeView extends StatelessWidget {
                       child: Obx(
                         () {
                           return SingleChildScrollView(
+                            controller: controller.scrollController,
                             child: !controller.cols.isNullOrBlank
-                                ? DataTable(
-                                    columns: controller.cols
-                                        .map(
-                                          (e) => DataColumn(
-                                            label: Text(
-                                              e.toString(),
-                                            ),
-                                          ),
-                                        )
-                                        .toList(),
-                                    rows: controller.rows
-                                        .map(
-                                          (item) => DataRow(
-                                            cells: item
-                                                .map(
-                                                  (e) => DataCell(
-                                                    Text(
-                                                      e.toString(),
-                                                    ),
-                                                  ),
-                                                )
-                                                .toList(),
-                                          ),
-                                        )
-                                        .toList(),
+                                ? Column(
+                                    children: [
+                                      DataTable(
+                                        columns: controller.cols
+                                            .map(
+                                              (e) => DataColumn(
+                                                label: Text(
+                                                  e.toString(),
+                                                ),
+                                              ),
+                                            )
+                                            .toList(),
+                                        rows: controller.rows
+                                            .map(
+                                              (item) => DataRow(
+                                                cells: item
+                                                    .map(
+                                                      (e) => DataCell(
+                                                        Text(
+                                                          e.toString(),
+                                                        ),
+                                                      ),
+                                                    )
+                                                    .toList(),
+                                              ),
+                                            )
+                                            .toList(),
+                                      ),
+                                    ],
                                   )
                                 : const SizedBox.shrink(),
                           );
@@ -115,3 +121,130 @@ class HomeView extends StatelessWidget {
     );
   }
 }
+
+/*
+import 'package:Decision_Tree_Generator/controllers/homeControllor.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeView extends StatelessWidget {
+  final HomeController controller = Get.put(HomeController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          controller.start.value = 0;
+          controller.openFileExplorer();
+        },
+        label: const Text('Upload'),
+        hoverElevation: 20.0,
+        icon: const Icon(Icons.upload_rounded),
+      ),
+      appBar: AppBar(
+        toolbarHeight: 60,
+        title: const Text(
+          'Decision Tree Generator',
+          textScaleFactor: 1.5,
+        ),
+        toolbarOpacity: 0.8,
+      ),
+      body: ListView(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              Obx(
+                () => IndexedStack(
+                  index: controller.index,
+                  children: [
+                    Container(
+                      color: Colors.tealAccent,
+                      height: 500,
+                      width: double.maxFinite,
+                      margin: const EdgeInsets.all(30.0),
+                    ),
+                    Container(
+                      color: Colors.deepPurpleAccent,
+                      height: 500,
+                      width: double.maxFinite,
+                      margin: const EdgeInsets.all(30.0),
+                      child: Obx(
+                        () {
+                          return SingleChildScrollView(
+                            controller: controller.scrollController,
+                            child: !controller.cols.isNullOrBlank
+                                ? Column(
+                                    children: [
+                                      DataTable(
+                                        columns: controller.cols
+                                            .map(
+                                              (e) => DataColumn(
+                                                label: Text(
+                                                  e.toString(),
+                                                ),
+                                              ),
+                                            )
+                                            .toList(),
+                                        rows: controller.rows
+                                            .map(
+                                              (item) => DataRow(
+                                                cells: item
+                                                    .map(
+                                                      (e) => DataCell(
+                                                        Text(
+                                                          e.toString(),
+                                                        ),
+                                                      ),
+                                                    )
+                                                    .toList(),
+                                              ),
+                                            )
+                                            .toList(),
+                                      ),
+                                    ],
+                                  )
+                                : const SizedBox.shrink(),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 9.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlineButton(
+                      borderSide: const BorderSide(color: Colors.blue),
+                      shape: const StadiumBorder(),
+                      onPressed: () {
+                        controller.index = 0;
+                      },
+                      child: const Text('Tree'),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    OutlineButton(
+                      borderSide: const BorderSide(color: Colors.blue),
+                      shape: const StadiumBorder(),
+                      onPressed: () {
+                        controller.index = 1;
+                      },
+                      child: const Text('Table'),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+   */
